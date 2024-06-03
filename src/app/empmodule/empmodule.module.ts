194e96d127f2e1febac2e10bacd8c079405de6a3
A Venkatesh <avenkateshanand1998@gmail.com>
2024-06-04 01:50:37 +0530

    Here i Have to create a forms

--- src/app/empmodule/empmodule.module.ts
+++ src/app/empmodule/empmodule.module.ts
@@ -1,9 +1,17 @@
 import { ModuleWithProviders, NgModule, Type } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 
-const materialUI:any[]= [MatToolbarModule,]
+
+const materialUI:any[]= [MatToolbarModule,
+                         MatGridListModule,
+                         MatFormFieldModule,
+                         MatInputModule,
+                        ]
 
 @NgModule({
   imports: [materialUI,
