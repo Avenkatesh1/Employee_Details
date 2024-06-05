821bc114d8bb72f20501fff070147f50536331be
A Venkatesh <avenkateshanand1998@gmail.com>
2024-06-05 12:47:07 +0530

    import Environments folder and installing the Firebase API Appilcation in the project

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebaseConfig :{
+        apiKey: "AIzaSyDJis9mJf3JtO5N_FU5y9SEUEhwR0TSExA",
+        authDomain: "employee-details-e9b31.firebaseapp.com",
+        projectId: "employee-details-e9b31",
+        storageBucket: "employee-details-e9b31.appspot.com",
+        messagingSenderId: "101835790852",
+        appId: "1:101835790852:web:ba66d45f92bdad5ab8f351",
+        measurementId: "G-F7ZN8RHCVV"
+      }
+};
